cf5875ee66c68f696d5758871531094e51f65bf0
Torin Stephen <30649929+torin-stephen@users.noreply.github.com>
2022-01-09 14:44:33 +0000

    commit to gh

---
+++ gen.py
@@ -0,0 +1,33 @@
+from airium import Airium
+a = Airium()
+
+#define variables
+date = ''
+cont = ''
+date2 = ''
+cont2 = ''
+
+with open('data.txt') as file:
+    cnt = 0
+    for line in file:
+        print(line)
+        date = line.partition("|")[0]
+        cont = line.partition("|")[2]
+        if (cnt % 2) == 0:
+            with a.div(klass='container left'):
+                with a.div(klass='content'):
+                    a.h2(_t=date)
+                    with a.p():
+                        a(cont)
+        else:
+            with a.div(klass='container right'):
+                with a.div(klass='content'):
+                    a.h2(_t=date)
+                    with a.p():
+                        a(cont)
+        cnt += 1
+
+html = str(a)
+
+out = open("output.html", "x")
+out.write(html)
